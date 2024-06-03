8db0bcd7ba9e6671e4e8b4d29b59bbf6c8527a42
[DaniellaBarraza125] <daniellabarraza125@gmail.com>
2024-06-03 13:07:33 +0200

    componentes agregados

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
-import Headers from './componets/Header/Headers'
+import Footer from './componets/Footer/Footer';
+import Home from './componets/Home/Home';
+import Login from './componets/Login/Login';
+import Post from './componets/Post/Post';
+import Profile from './componets/Profile/Profile';
+import Register from './componets/Register/Register';
+import SideBar from './componets/SideBar/SideBar';
 
 function App() {
 
 
   return (
     <>
-    <Headers/>
-  
-      
+    <BrowserRouter>
+      <SideBar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path='Post/:id' element={<Post/>}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+
     </>
   )
 }
---
+++ src/componets/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Posts from '../Posts/Posts'
+import CreatePost from '../CreatePost/CreatePost'
+
+const Home = () => {
+  return (
+    <div>
+        soy el home
+    </div>
+  )
+}
+
+export default Home
