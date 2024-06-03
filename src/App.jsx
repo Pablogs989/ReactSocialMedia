ef3efb00fa0d5125e94e36462512654266aee36d
[DaniellaBarraza125] <daniellabarraza125@gmail.com>
2024-06-03 12:23:27 +0200

    iniciando proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.scss'
+import { Footer } from './componets/Footer/Footer'
+import Headers from './componets/Header/Headers'
+
+function App() {
+
+
+  return (
+    <>
+    <Headers/>
+    <Footer/>
+      
+    </>
+  )
+}
+
+export default App
