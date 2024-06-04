99ab06d98d1aa37fdc5dc1e6840a78735b4a9edf
[DaniellaBarraza125] <daniellabarraza125@gmail.com>
2024-06-04 12:27:47 +0200

    register agregado

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_URL = "http://localhost:8080/users";
+
 const register = async (userData) => {
     const res = await axios.post(API_URL + "/users", userData);
     return res.data;
