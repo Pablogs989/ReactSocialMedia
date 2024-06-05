a0e4e11e2bcab40454597820b19ab8a0f84aa71e
Pablogs989 <pablogonsan2001@gmail.com>
2024-06-05 14:00:57 +0200

    post card

--- src/componets/Post/Post.jsx
+++ src/componets/Post/Post.jsx
@@ -1,18 +1,46 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import "./Post.scss";
 
 const Post = () => {
   const { posts } = useSelector((state) => state.posts);
 
   const post = posts.map((post) => {
-    return (
-      <div className="post" key={post._id}>
-        <Link to={"/post/" + post._id}>
-          <p>{post.text}</p>
-        </Link>
-      </div>
-    );
+    console.log(post.userId);
+    if (!post.userId) return null;
+
+    if (post.image === null || post.image === "" || post.image === undefined) {
+      console.log(!post.userId);
+      return (
+        <div className="card" key={post._id}>
+          <Link to={"/post/" + post._id}>
+            <p className="card-body">{post.text}</p>
+            <p className="footer">
+              Written by <span className="by-name">{post.userId.name}</span> on{" "}
+              <span className="date">{post.createdAt.slice(0, 10)}</span>
+            </p>
+          </Link>
+        </div>
+      );
+    } else {
+      return (
+        <div className="card" key={post._id}>
+          <Link to={"/post/" + post._id}>
+            <div className="card-image">
+              <img className="img"
+                src={"http://localhost:8080/public/posts/" + post.image}
+              ></img>
+            </div>
+            <p className="card-body">{post.text}</p>
+            <p className="footer">
+              Written by <span className="by-name">{post.userId.name}</span> on{" "}
+              <span className="date">{post.createdAt.slice(0, 10)}</span>
+            </p>
+          </Link>
+        </div>
+      );
+    }
   });
   return <div>{post}</div>;
 };
--- src/componets/Posts/Posts.jsx
+++ src/componets/Posts/Posts.jsx
@@ -16,7 +16,6 @@ const Posts = () => {
 
   return (
     <div>
-      <h1>Posts</h1>
       <Post />
     </div>
   );
