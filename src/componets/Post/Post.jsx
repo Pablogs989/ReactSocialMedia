26025d9865ad7ddeb754d22526c9d5e877760baf
Dani <162794640+DaniellaBarraza125@users.noreply.github.com>
2024-06-06 11:42:23 +0200

    post update

--- src/componets/Post/Post.jsx
+++ src/componets/Post/Post.jsx
@@ -6,37 +6,28 @@ const Post = ({posts}) => {
   const post = posts.map((post) => {
     if (!post.userId) return null;
 
-    if (post.image === null || post.image === "" || post.image === undefined) {
-      return (
-        <div className="card" key={post._id}>
-          <Link to={"/post/" + post._id}>
-            <p className="card-body">{post.text}</p>
-            <p className="footer">
-              Written by <span className="by-name">{post.userId.name}</span> on{" "}
-              <span className="date">{post.createdAt.slice(0, 10)}</span>
-            </p>
-          </Link>
-        </div>
-      );
-    } else {
-      return (
-        <div className="card" key={post._id}>
-          <Link to={"/post/" + post._id}>
-            <div className="card-image">
-              <img className="img"
+    return (
+      <div className="card" key={post._id}>
+        <Link to={"/post/" + post._id}>
+          <div className="card-image">
+            {post.image && (
+              <img
+                className="img"
                 src={"http://localhost:8080/public/posts/" + post.image}
-              ></img>
-            </div>
-            <p className="card-body">{post.text}</p>
-            <p className="footer">
-              Written by <span className="by-name">{post.userId.name}</span> on{" "}
-              <span className="date">{post.createdAt.slice(0, 10)}</span>
-            </p>
-          </Link>
-        </div>
-      );
-    }
-  });
+                alt="Post"
+              />
+            )}
+          </div>
+          <p className="card-body">{post.text}</p>
+          <p className="footer">
+            Written by <span className="by-name">{post.userId.name}</span> on{" "}
+            <span className="date">{post.createdAt.slice(0, 10)}</span>
+          </p>
+        </Link>
+      </div>
+    );
+  }
+  );
   return <div>{post}</div>;
 };
 
