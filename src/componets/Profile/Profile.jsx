cf0f224bcda4b2b1ee37e24f0cff3dee137d6476
Pablogs989 <pablogonsan2001@gmail.com>
2024-06-06 12:37:45 +0200

    localsotage user

--- src/componets/Profile/Profile.jsx
+++ src/componets/Profile/Profile.jsx
@@ -1,33 +1,30 @@
-import { Spin } from 'antd';
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import UserInfo from '../UserInfo/UserInfo';
-import './Profile.scss';
-import { getUserInfo } from '../../features/auth/authSlice';
-import Post from '../Post/Post';
-import GetUsers from '../GetUsers/GetUsers';
+import { Spin } from "antd";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import UserInfo from "../UserInfo/UserInfo";
+import "./Profile.scss";
+import { getUserInfo } from "../../features/auth/authSlice";
+import Post from "../Post/Post";
+import GetUsers from "../GetUsers/GetUsers";
 
 const Profile = () => {
-  const  { user } = useSelector((state)=>state.auth)
-  const dispatch = useDispatch()
-  
+  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  console.log("profileuser", user);
   useEffect(() => {
-    dispatch((getUserInfo()))
-  }, 
-  [])
+    dispatch(getUserInfo());
+  }, []);
   if (!user) {
     return <Spin />;
   }
   return (
-    <div className='profileDivContainer'>
-      <UserInfo/>
+    <div className="profileDivContainer">
+      <UserInfo />
       <div className="postProfileDiv">
-
-      <Post posts={user.postsId}/>
+        <Post posts={user.postsId} />
       </div>
-    
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
