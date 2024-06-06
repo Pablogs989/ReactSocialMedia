4ed33a8be76cc222dd68daf736801dfcd285a719
Pablogs989 <pablogonsan2001@gmail.com>
2024-06-06 11:16:36 +0200

    posts screens

--- src/componets/Post/Post.jsx
+++ src/componets/Post/Post.jsx
@@ -1,49 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
-import { Link, useLocation } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 import "./Post.scss";
 
 const Post = () => {
-  const { posts, userPosts } = useSelector((state) => state.posts);
-  const location = useLocation();
-  const [postsArray, setPostsArray] = useState([]);
-
-  useEffect(() => {
-    switch (location.pathname) {
-      case "/following":
-      case "/profile":
-        setPostsArray(userPosts);
-        break;
-      case "/":
-        setPostsArray(posts);
-        break;
-      default:
-        break;
-    }
-  }, [location.pathname, posts, userPosts]);
+  const { posts } = useSelector((state) => state.posts);
 
   return (
     <div className="post-container">
-      {postsArray.map((post) => {
+      {posts.map((post) => {
         if (!post.userId) return null;
 
         return (
           <div className="card" key={post._id}>
             <Link to={"/post/" + post._id}>
-              {post.image && (
-                <div className="card-image">
+              <div className="card-image">
+                {post.image && (
                   <img
                     className="img"
                     src={"http://localhost:8080/public/posts/" + post.image}
                     alt="Post"
                   />
-                </div>
-              )}
+                )}
+              </div>
               <p className="card-body">{post.text}</p>
               <p className="footer">
-                Written by <span className="by-name">{post.userId.name}</span>{" "}
-                on <span className="date">{post.createdAt.slice(0, 10)}</span>
+                Written by <span className="by-name">{post.userId.name}</span> on{" "}
+                <span className="date">{post.createdAt.slice(0, 10)}</span>
               </p>
             </Link>
           </div>
