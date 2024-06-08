273c50374f2ff82a2d232b5cea14deba7e819ac8
Pablogs989 <pablogonsan2001@gmail.com>
2024-06-08 11:37:35 +0200

    Update PostDetail.jsx

--- src/componets/PostDetail/PostDetail.jsx
+++ src/componets/PostDetail/PostDetail.jsx
@@ -13,11 +13,27 @@ const PostDetail = () => {
     dispatch(getById(id));
   }, []);
 
+  console.log(post);
+  
   return (
     <div>
-      <h1>PostDetail</h1>
-      <p>{post._id}</p>
-      <p>{post.text}</p>
+      {post.image && (
+        <div className="card-image">
+          <img
+            className="img"
+            src={"http://localhost:8080/public/posts/" + post.image}
+            alt="Post"
+          />
+        </div>
+      )}
+      <div className="card-body">
+        {post.text}
+      </div>
+      <div className="footer">
+        <div className="by-name">Written by {post.userId.name} </div>{" "}
+        <span>on </span>
+        <div className="date"> {post.createdAt.slice(0, 10)}</div>
+      </div>
     </div>
   );
 };
