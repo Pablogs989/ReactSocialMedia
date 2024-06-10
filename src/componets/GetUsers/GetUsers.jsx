488f642ddc1c831c7c6c52f2fd2cb5f7db7bdf90
Pablogs989 <pablogonsan2001@gmail.com>
2024-06-10 20:25:47 +0200

    search user

--- src/componets/GetUsers/GetUsers.jsx
+++ src/componets/GetUsers/GetUsers.jsx
@@ -1,28 +1,41 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getUsers } from '../../features/auth/authSlice'
-import UserCard from '../UserCard/UserCard'
-import "./GetUsers.scss"
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUsers } from "../../features/auth/authSlice";
+import UserCard from "../UserCard/UserCard";
+import "./GetUsers.scss";
+import { Input } from "antd";
 
 const GetUsers = () => {
-    const dispatch = useDispatch()
-    const users = useSelector(state => state.auth.users)
-    
-    
-    useEffect(() => {
-        dispatch(getUsers())
-    }, [dispatch])
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.auth.users);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    return (
-        <div className='getUsersDivContainer'>
-            <div className="userCard">
-                <div>
-                    <UserCard users={users}/> 
-                </div>
-                <a className="moreUserCard" href="#">See more</a>
-            </div>
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
+
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="getUsersDivContainer">
+      <Input
+        type="text"
+        placeholder="Search by post text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <div className="userCard">
+        <div>
+          <UserCard users={filteredUsers} />
         </div>
-    )
-}
+        <a className="moreUserCard" href="#">
+          See more
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default GetUsers
+export default GetUsers;
